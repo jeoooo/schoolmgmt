2e5f7403343a1f855028cdd8c3ab15e330eb4e4a
jeoooo <jeocarlolubao@gmail.com>
2025-01-21 16:46:55 +0800

    feat: Update CoursesSerializer to handle department_id for create and update methods

--- courses/api/v1/serializers.py
+++ courses/api/v1/serializers.py
@@ -3,21 +3,31 @@ from courses.models import Course
 from departments.api.v1.serializers import DepartmentSerializer
 
 class CoursesSerializer(serializers.ModelSerializer):
-    department = DepartmentSerializer(read_only=True)
     department_name = serializers.CharField(source='department.name', read_only=True)
-    department_id = serializers.IntegerField(source='department.id', read_only=True)
-    
+    department_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Course
         fields = [
             'id', 
-            'department', 
             'department_id', 
             'department_name', 
             'name', 
+            'code',
             # 'description', 
         ]
         read_only_fields = [
             'date_created', 
             'date_updated'
         ]
+
+    def create(self, validated_data):
+        department_id = validated_data.pop('department_id')
+        course = Course.objects.create(department_id=department_id, **validated_data)
+        return course
+
+    def update(self, instance, validated_data):
+        department_id = validated_data.pop('department_id', None)
+        if department_id is not None:
+            instance.department_id = department_id
+        return super().update(instance, validated_data)
