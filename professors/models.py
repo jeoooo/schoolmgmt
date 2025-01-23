e7a132beefe53496e617b8d3b9e5a429680a792e
jeoooo <jeocarlolubao@gmail.com>
2025-01-23 17:56:18 +0800

    fix: Update ForeignKey reference to Department model in Professor model

--- professors/models.py
+++ professors/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from departments.models import Department
 # Create your models here.
 class Professor(models.Model):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='professors')
+    department = models.ForeignKey('departments.Department', on_delete=models.CASCADE, related_name='professors')
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     specialization = models.CharField(max_length=255)
     contact_number = models.CharField(max_length=15)
+    # subjects = models.ManyToManyField(Subject, related_name='professors')  # Professors teach multiple subjects
 
     class Meta:
         verbose_name = _("Professor")
