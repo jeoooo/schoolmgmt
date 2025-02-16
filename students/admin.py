3cf2d74301a1dfd8885e8e73f9540f9e1c78fa4b
JEO CARLO LUBAO <jeocarlolubao@gmail.com>
2025-02-16 16:47:41 +0800

    feat: Update StudentAdmin to display additional fields and improve filtering options

--- students/admin.py
+++ students/admin.py
@@ -3,7 +3,7 @@ from students.models import Student
 
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'department', 'roll_number', 'date_created', 'date_updated')
-    list_filter = ('department',)    
+    list_display = ('id', 'first_name', 'last_name', 'student_id', 'email', 'contact_number', 'department')
+    list_filter = ('department',)
 
-admin.site.register(Student, StudentAdmin)    
+admin.site.register(Student, StudentAdmin)
